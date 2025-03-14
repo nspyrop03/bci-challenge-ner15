0c4145559afafc30f9141fbb0ccad383aedd1bda
Nikolas Spyropoulos <nick.spyrop03@gmail.com>
2025-03-14 22:01:47 +0200

    wrote a good method for ErrP detection

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 
+# List of important channels for the P300 speller paradigm as found in bibliography
+important_channels = ['Cz', 'Pz', 'FCz', 'Fz', 'C1', 'CP1', 'CPz', 'CP2', 'C2', 'C3', 'FC3', 'P1', 'FC1', 'FC2', 'F1', 'C4']
+
 def bandpass_filter_all(part, lowcut = 1, highcut = 40, fs = 200, order = 5):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
@@ -14,14 +17,22 @@ def bandpass_filter_all(part, lowcut = 1, highcut = 40, fs = 200, order = 5):
         filtered_part[channel] = filtfilt(b, a, part[channel])
     return filtered_part
 
-def get_part_after_feedback(data, feedback_times, feedback_id = 0):
+# The paper that describes the experiment setup says that the letter chosen is displayed for 1.3s => after = 1.3 by default
+def get_part_after_feedback(data, feedback_times, feedback_id = 0, before = 0.2, after = 1.3):
     t = feedback_times.iloc[feedback_id]
-    return data[(data['Time'] >= t - 0.2) & (data['Time'] <= t + 1.0)], t
+    return data[(data['Time'] >= t - before) & (data['Time'] <= t + after)], t
 
-def plot_channel(part, t, channel, title = None):
+def plot_channel(part, t, channel = important_channels[0], title = None):
     time = (part['Time'] - t) * 1000
     plt.plot(time, part[channel])
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
+    plt.title(title)
+    plt.show()
+
+def plot_signal(signal, ylabel = 'Amplitude', title = None):
+    plt.plot(signal)
+    plt.xlabel('Samples')
+    plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
